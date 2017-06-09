5eb09e4425850528eaa1fb97a8015bdf83b95c67
Alex Gaudio <adgaudio@gmail.com>
2017-06-08 21:13:27 -0400

    rename 2020_connectors to OpenSCAD_connectors and make its own repo

--- 2020_connectors/generate_images.sh
+++
@@ -1,9 +0,0 @@
-# local params=" --imgsize=800,800 --camera=186,40,20,34,0,341,636"
-
-
-
-files=( "connector.scad" "tjoint.scad" "corner_bracket.scad" )
-for scad_file in "${files[@]}" ; do
-openscad --imgsize=550,500 --camera=20,0,0,45,0,25,800 -o images/${scad_file/.scad/.png} $scad_file
-done
-
